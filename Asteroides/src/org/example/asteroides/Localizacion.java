eb6dbcfc83d4e35db4786a93964b1d3d72dc4907
glopezGitHub <guillermo.pando@gmail.com>
2013-02-16 20:16:02 +0100

    Nueva funcionalidad para menu land

--- Asteroides/src/org/example/asteroides/Localizacion.java
+++ Asteroides/src/org/example/asteroides/Localizacion.java
@@ -4,29 +4,61 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Localizacion extends Activity {
 
-	/*main*/
+	Button acercaDe;
+
+	/* main */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_localizacion);
+
+		acercaDe = (Button) findViewById(R.id.button4);
+
+		acercaDe.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				// Finish
+				finish();
+
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_localizacion, menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
 		return true;
+		/** true -> el menú ya está visible */
 	}
 
-	  public void lanzarAcercaDe(View view){
+	public void lanzarAcercaDe(View view) {
 
-	      Intent i = new Intent(this, AcercaDe.class);
+		Intent i = new Intent(this, AcercaDe.class);
 
-          startActivity(i);
+		startActivity(i);
 
-	    }
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.acercaDe:
+			lanzarAcercaDe(null);
+			break;
+		}
+		return true;
+		/** true -> consumimos el item, no se propaga */
+	}
 }
